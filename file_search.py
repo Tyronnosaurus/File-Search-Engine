02093019ce164b973c3e4957cc9f62336349a870
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-05-29 23:08:16 +0200

    Added class skeleton

--- file_search.py
+++ file_search.py
@@ -2,3 +2,22 @@ import os
 import pickle
 
 class SearchEngine:
+    def __init__(self):
+        self.file_index = []
+        self.results = []
+        self.matches = 0
+        self.records = 0
+
+    def create_new_index(self, root_path):
+        #Create a new index and save to a file
+        pass
+
+    def load_existing_index(self):
+        #Load existing index
+        pass
+
+    defsearch(self, term, search:_type = 'contains'):
+        #Search for term based on search type
+        pass
+
+    
