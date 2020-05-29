40433cc9520bb8f7b016b9e23b367c8c6919e58d
Tyronnosaurus <Tyronnosaurus@protonmail.com>
2020-05-30 00:01:11 +0200

    Implemented SearchEngine class' functions

--- file_search.py
+++ file_search.py
@@ -10,14 +10,47 @@ class SearchEngine:
 
     def create_new_index(self, root_path):
         #Create a new index and save to a file
-        pass
+        self.file_index = [(root, files) for root, dirs, files in os.walk(root_path) if files]  #'if files' filters out empty file list
+
+        #Save to file
+        with open('file_index.pkl' , 'wb') as f:    #Open as 'write binary'
+            pickle.dump(self.file, f)
+
 
     def load_existing_index(self):
         #Load existing index
-        pass
+        try:
+            with open('file_index.pkl','rb') as f:  #Open as 'read binary'
+                self.file_index = pickle.load(f)
+        except:                                     #In case file doesn't exist
+            self.file_index = []
 
-    defsearch(self, term, search:_type = 'contains'):
+
+    def search(self, term, search_type = 'contains'):
         #Search for term based on search type
-        pass
+        
+        #Reset variables
+        self.results.clear()
+        self.matches = 0
+        self.records = 0
 
-    
+        #Perform search
+        for path, files in self.file_index:
+            for file in files:
+                self.records += 1
+                if ((search_type == 'contains' and term.lower() in file.lower()) or
+                    (search_type == 'startswith' and file.lower().startswith(term.lower())) or
+                    (search_type == 'endswith' and file.lower().endswith(term.lower()))):
+                    
+                    result = path.replace('\\' , '/') + '/' + file
+                    self.results.append(result)
+                    self.matches += 1
+                else:
+                    continue
+
+        #Save search results
+        with open('search_results.txt','w') as f:
+            for row in self.results:
+                f.write(row + '\n')
+
+
